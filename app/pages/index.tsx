d9e4d3f296ea6476acff985925876cb72e087ad3
dhruvraj <dhruvrajsinghsolanki161@gmail.com>
2022-04-23 12:30:19 +0530

    feat: nextjs tailwind app setup done

---
+++ app/pages/index.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from 'next'
+
+
+const Home: NextPage = () => {
+  return (
+   <>
+   <h1>Hello</h1>
+   <h2>Welcome to Sol Blog</h2>
+   </>
+  )
+}
+
+export default Home
